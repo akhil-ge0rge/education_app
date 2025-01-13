7798b4ebb9beae49f0fd62c977f137427e87cc98
Akhil George <akhil8311@gmail.com>
2025-01-13 14:40:04 +0530

    firebase congif

--- lib/src/auth/data/datasources/auth_remote_datasources.dart
+++ lib/src/auth/data/datasources/auth_remote_datasources.dart
@@ -1,5 +1,6 @@
 import 'package:education_app/core/enums/update_user.dart';
 import 'package:education_app/src/auth/data/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class AuthRemoteDatasources {
   const AuthRemoteDatasources();
@@ -22,4 +23,17 @@ abstract class AuthRemoteDatasources {
     required UpdateUserAction action,
     required dynamic userData,
   });
+}
+
+class AuthRemoteDatasourcesImpl implements AuthRemoteDatasources {
+  AuthRemoteDatasourcesImpl({
+    required FirebaseAuth authClient,
+    required FirebaseFirestore cloudStorageClient,
+    required FirebaseStorage dbClient,
+  })  : _authClient = authClient,
+        _cloudStorageClient = cloudStorageClient,
+        _dbClient = dbClient;
+  final FirebaseAuth _authClient;
+  final FirebaseFirestore _cloudStorageClient;
+  final FirebaseStorage _dbClient;
 }
